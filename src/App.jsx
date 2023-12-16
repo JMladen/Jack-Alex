af4c19d7f70fb07292aa0082cca5d0f2d3b3a4a0
Jack Mladen <jackmladen1@gmail.com>
2023-12-15 18:04:20 -0800

    fuck me up fam

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Alex + Jack</h1>
+      <h1>Alex + Jack!!!!!!!!!!!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
